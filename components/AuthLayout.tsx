36eb1f87da3ad7a11bafa308ca5051830b87c9ff
SaltifySaas <saltifysaas@gmail.com>
2025-07-20 03:55:03 +0530

    add saltify logo image

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import Image from "next/image";
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen bg-[#cbddd1] flex items-center justify-center">
       <div className="w-[500px] p-4 rounded-xl bg-[#0f2b22] text-white">
         {/* Brand Header */}
         <div className="flex justify-center mb-8">
-          <h1 className="text-4xl font-semibold">Saltify</h1>
+          <Image
+            src="/yuvraj/saltify-frontend/public/logo/saltify.png"
+            alt="Saltify Logo"
+            width={180}
+            height={60}
+            priority
+          />
         </div>
 
         {children}
