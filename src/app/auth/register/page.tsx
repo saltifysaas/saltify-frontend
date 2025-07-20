b8195e8a18bbddba8211d80f32e6e32aa4a123ce
SaltifySaas <saltifysaas@gmail.com>
2025-07-21 01:16:22 +0530

    chore: update dev with latest changes

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,7 @@
 import AuthLayout from "@/components/AuthLayout";
 import RegisterForm from "@/components/RegisterForm";
+console.log('API endpoint is:', process.env.NEXT_PUBLIC_API_URL);
+
 
 export default function RegisterPage() {
   return (
